2def6e978da27cf506d265f1f1732e4329d15547
Francisco Clavero <k2.francisco@grupofleury.com.br>
2026-09-22 23:13:54 -0300

    feat: add NotFoundException Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -20,7 +20,7 @@ export class UsersService {
   async findOneOrFail(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     return user;
   }

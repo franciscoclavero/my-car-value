0e85fe4781294adf2f508c0ec2216f69de1beb3d
Francisco Clavero <k2.francisco@grupofleury.com.br>
2026-09-23 22:00:20 -0300

    feat: add argon2 to hash and add hash into password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+
+    if (users.length) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const passwordHash = await argon2.hash(password);
+
+    return this.usersService.create(email, passwordHash);
+  }
+
+  signin() {}
 }

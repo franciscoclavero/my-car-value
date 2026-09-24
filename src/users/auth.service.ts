19c9ba1082164b4d7b3a211a3d417f02c266a296
Francisco Clavero <k2.francisco@grupofleury.com.br>
2026-09-23 22:23:36 -0300

    feat: created login

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import * as argon2 from 'argon2';
 
@@ -20,5 +24,19 @@ export class AuthService {
     return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const passwordMatches = await argon2.verify(user.password, password);
+
+    if (!passwordMatches) {
+      throw new BadRequestException('Invalid password');
+    }
+
+    return user;
+  }
 }
